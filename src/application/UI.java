d62184f510bc461f0cff3b478113388b202884a4
Vinicius Fernandes <viniviniciusfernandes15@gmail.com>
2024-07-08 22:40:49 -0300

    adicionando turnos e alternando de jogador a cada turno

--- src/application/UI.java
+++ src/application/UI.java
@@ -3,6 +3,7 @@ package application;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -46,7 +47,7 @@ public class UI {
 
 	private static void printPiece(ChessPiece piece, boolean background) {
 		if (background) {
-			System.out.print(ANSI_PURPLE_BACKGROUND);
+			System.out.print(ANSI_BLUE_BACKGROUND );
 		}
 		if (piece == null) {
 			System.out.print("-" + ANSI_RESET);
@@ -59,6 +60,13 @@ public class UI {
 		}
 		System.out.print(" ");
 	}
+	
+	public static void printMatch(ChessMatch chessMatch) {
+		printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.println("Turn : " + chessMatch.getTurn());
+		System.out.println("Wating player: " + chessMatch.getCurrentPlayer());
+	} 
 
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
