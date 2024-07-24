00b2291c8f89aea3c67783044bbce1e7f5b6802b
Vinicius Fernandes <viniviniciusfernandes15@gmail.com>
2024-07-23 22:11:52 -0300

    Cavalo

--- src/application/UI.java
+++ src/application/UI.java
@@ -72,7 +72,6 @@ public class UI {
 		System.out.println("Turn : " + chessMatch.getTurn());
 		if (!chessMatch.getCheckMate()) {
 			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
 			if (chessMatch.getCheck()) {
 				System.out.println("CHECK!!");
 			}
